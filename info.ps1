6428133fe7917c759f9ba4450039e178e20d577c
Alex Tempel <alex@tempel-home.de>
2024-03-23 12:36:00 +0100

    posh to add folders

--- info.ps1
+++ info.ps1
@@ -1,1 +1,8 @@
 # info besides readme
+
+New-Item -Path . -Name "source" -ItemType "directory"
+
+New-Item -Path .\source\public -Name "source" -ItemType "directory"
+New-Item -Path .\source\private -Name "source" -ItemType "directory"
+New-Item -Path .\source\classes -Name "source" -ItemType "directory"
+New-Item -Path .\source\enum -Name "source" -ItemType "directory"
